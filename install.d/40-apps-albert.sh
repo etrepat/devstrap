9f0de1dd0a3de17fb80dd83d51a2e6c1647cb5db
Estanislau Trepat <estanis.trepat@inson.es>
2024-06-26 18:42:29 +0200

    Add albert launcher

---
+++ install.d/40-apps-albert.sh
@@ -0,0 +1,18 @@
+# Albert launcher
+# https://albertlauncher.github.io/
+
+m "Installing Albert launcher..."
+
+UBUNTU_VERSION_ID=$(. /etc/os-release && echo "$VERSION_ID")
+
+# Add gpg key
+curl -fsSL "https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_${UBUNTU_VERSION_ID}/Release.key" | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+
+# Add osb repository
+echo "deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_${UBUNTU_VERSION_ID}/ /" | \
+    sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y albert
