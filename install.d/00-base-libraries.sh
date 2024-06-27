a9b49ea4b2e575cc1b44027ee2611f8c4dcbf006
Estanislau Trepat <estanis.trepat@inson.es>
2024-06-27 11:26:34 +0200

    Add support tools

--- install.d/00-base-libraries.sh
+++ install.d/00-base-libraries.sh
@@ -2,5 +2,5 @@
 
 m "Installing base packages & libraries..."
 sudo apt-get install -y git curl wget zip unzip build-essential pkg-config \
-    autoconf bison libssl-dev libreadline-dev zlib1g-dev libyaml-dev \
-    libreadline-dev libncurses5-dev libffi-dev
+    autoconf bison clang cmake imagemagick libssl-dev libreadline-dev \
+    zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev
