e365856457065897be4617d6e904f52b4a5dfbe3
Estanislau Trepat <estanis.trepat@inson.es>
2024-06-27 11:50:29 +0200

    Add more base tools & libs

--- install.d/00-base-libraries.sh
+++ install.d/00-base-libraries.sh
@@ -2,5 +2,6 @@
 
 m "Installing base packages & libraries..."
 sudo apt-get install -y git curl wget zip unzip build-essential pkg-config \
-    autoconf bison clang cmake imagemagick libssl-dev libreadline-dev \
-    zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev
+    autoconf bison clang cmake imagemagick sqlite3 libsqlite3-0 libsqlite3-dev \
+    postgresql-client redis-tools libpq-dev libssl-dev libreadline-dev \
+    zlib1g-dev libyaml-dev libncurses5-dev libffi-dev
