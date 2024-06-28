398fe39a31e5e5b8ae0d8978b03ea9a3ea95f663
Estanislau Trepat <estanis.trepat@inson.es>
2024-06-28 13:46:52 +0200

    Add git settings

---
+++ install.d/10-utils-git.sh
@@ -0,0 +1,10 @@
+# Set common git aliases
+
+m "Applying some git config..."
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+git config --global pull.rebase true
