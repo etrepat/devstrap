8dd05929d13927eafc960d7fc4e56a22eca1998d
Estanislau Trepat <estanis@etrepat.com>
2025-02-26 22:05:20 +0100

    Add git configuration file and update installation script

--- install.d/10-utils-git.sh
+++ install.d/10-utils-git.sh
@@ -2,9 +2,10 @@
 
 echo "=> Applying some git config..."
 
+[ -f "~/.gitconfig" ] && mv ~/.gitconfig ~/.gitconfig.bak
+cp -f "${DEVSTRAP_PATH}/config/gitconfig" ~/.gitconfig
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-
-git config --global pull.rebase true
