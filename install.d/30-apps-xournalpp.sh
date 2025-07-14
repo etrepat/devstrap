114bcb7a9b9c4e1f4ff6b3696803013ca09d2973
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-14 18:54:14 +0200

    Add xournalpp to installation

---
+++ install.d/30-apps-xournalpp.sh
@@ -0,0 +1,5 @@
+# Xournal++ - note-taking application that is designed for handwriting, sketching, and annotating PDFs.
+# https://xournalpp.github.io/
+
+echo "=> Intalling Xournal++..."
+sudo apt-get install -y xournalpp
