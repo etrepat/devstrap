8f6a9e9812885803c027f4d4b6153507cd231754
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-15 16:14:39 +0200

    Add missing term tools

--- install.d/10-utils-terminal.sh
+++ install.d/10-utils-terminal.sh
@@ -1,4 +1,4 @@
 # Use command-line utilities
 
 echo "=> Installing command-line utilities..."
-sudo apt-get install -y fzf bat eza btop xclip jq
+sudo apt-get install -y fzf ripgrep bat eza btop xclip jq fd-find
