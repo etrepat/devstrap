07d9445d9faf6dda7fed19d6d4d747c86e29c204
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-25 12:06:18 +0200

    Add albert launcher to autostart

--- install.d/30-apps-albert.sh
+++ install.d/30-apps-albert.sh
@@ -4,3 +4,9 @@
 
 echo "=> Installing Albert launcher..."
 yay -S --noconfirm --needed albert
+
+# Add to autostart
+mkdir -p ~/.config/autostart
+cp -f /usr/share/applications/albert.desktop ~/.config/autostart/albert.desktop
+sed -i '${/^$/d;}' ~/.config/autostart/albert.desktop && \
+    echo 'X-GNOME-Autostart-enabled=true' >> ~/.config/autostart/albert.desktop
