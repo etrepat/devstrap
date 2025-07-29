77c1bae66a11d1ddc306ca902981f49b5269f01b
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-29 17:29:21 +0200

    Remove htop before installing btop

--- install.d/00-base-apps-and-pkgs.sh
+++ install.d/00-base-apps-and-pkgs.sh
@@ -3,8 +3,6 @@
 
 echo "=> Installing base packages & libraries..."
 
-yay -Rns --noconfirm htop
-
 yay -S --needed --noconfirm autoconf base-devel bash-completion bison \
     cmake clang curl git imagemagick man less libffi llvm pkgconf \
     postgresql-libs openssl readline sqlite unzip wget whois xclip zip zlib
