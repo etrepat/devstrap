5b81037e3faa5d8126d47cfb089501e3a82e791f
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-29 11:58:39 +0200

    Only require git, at first.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ DEVSTRAP_PATH="${DEVSTRAP_TMP}/devstrap"
 echo -e "\e[33;1mdevstrap\e[0m"
 
 echo "=> Cloning devstrap scripts..."
-if ! command -v git &> /dev/null; then sudo pacman -S --noconfirm --needed base-devel git > /dev/null; fi
+if ! command -v git &> /dev/null; then sudo pacman -S --noconfirm --needed git > /dev/null; fi
 [ -d "${DEVSTRAP_PATH}" ] && rm -fr ${DEVSTRAP_PATH}
 git clone -b archlinux https://github.com/etrepat/devstrap.git ${DEVSTRAP_PATH} > /dev/null
 
