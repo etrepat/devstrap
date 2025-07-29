d1a8d889ec19642a87ccfb4d738c7a8a3bcb4029
Estanislau Trepat <estanis.trepat@inson.es>
2025-07-29 19:43:04 +0200

    Do not run gum installer if present

--- requirements.d/00-base-gum.sh
+++ requirements.d/00-base-gum.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Gum is used as TUI frontend for confirmations, option selection & messaging
 
-sudo pacman -S --noconfirm --needed gum
+if ! command -v gum &> /dev/null; then
+    sudo pacman -S --noconfirm --needed gum
+fi
