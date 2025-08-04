a6662ed67c11904979dbf15830ca5f270fa04027
Estanislau Trepat <estanis@etrepat.com>
2025-08-04 23:50:36 +0200

    Manjaro defaults to zsh

--- install.d/00-base-shell.sh
+++ install.d/00-base-shell.sh
@@ -6,8 +6,8 @@ echo "=> Copying bash settings (~/.bashrc)..."
 [ -f "~/.bash_aliases" ] && mv ~/.bash_aliases ~/.bash_aliases.bak
 cp -f "${DEVSTRAP_PATH}/config/bash_aliases" ~/.bash_aliases
 
-[ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp -f "${DEVSTRAP_PATH}/config/bashrc" ~/.bashrc
+[ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
+cp -f "${DEVSTRAP_PATH}/config/zshrc" ~/.zshrc
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
 cp "${DEVSTRAP_PATH}/config/inputrc" ~/.inputrc
