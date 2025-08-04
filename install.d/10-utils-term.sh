b3e7a0c891752d174852327e230981cd81d44506
Estanislau Trepat <estanis@etrepat.com>
2025-08-04 23:52:01 +0200

    Add chafa utility
    Add chafa for fast terminal image previews

--- install.d/10-utils-term.sh
+++ install.d/10-utils-term.sh
@@ -5,4 +5,4 @@ echo "=> Installing terminal utilities..."
 
 yay -Qq htop && yay -Rns --noconfirm htop
 
-yay -S --needed --noconfirm bat btop eza fastfetch fd fzf jq tldr
+yay -S --needed --noconfirm bat btop chafa eza fastfetch fd fzf jq tldr
