ce57015f737877052cf6d572b09286b995a3715d
Estanislau Trepat <estanis@etrepat.com>
2025-08-04 23:54:32 +0200

    Fix comments

--- install.d/30-apps-dbeaver.sh
+++ install.d/30-apps-dbeaver.sh
@@ -4,10 +4,6 @@
 # We install a custom-made aur package (pkgbuild) so that it can be installed
 # in a self-contained way, with its own JDK.
 
-# FIXME
-# This is to install a fully self-contained dbeaver version so that Java is included and can coexist with anything
-# installed by the user. We should think about removing this script and use an AUR package instead or provide a PKGBUILD.
-
 echo "=> Installing DBeaver..."
 
 cd ${DEVSTRAP_TMP}/pkg/dbeaver
