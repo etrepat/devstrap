33e25e6337e6fe68c6255568033b4c5c7c2ba61c
Estanislau Trepat <estanis.trepat@inson.es>
2025-08-05 18:59:41 +0200

    Add fastfetch

---
+++ install.d/10-utils-fastfetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# fastfetch - A maintained, feature-rich and performance oriented, neofetch like system information tool.
+# https://github.com/fastfetch-cli/fastfetch
+
+sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+sudo apt update -y
+sudo apt install -y fastfetch
