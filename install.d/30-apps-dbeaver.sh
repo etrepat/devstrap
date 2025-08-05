ba6cafffc651c425a328ff8551549b262a74c0b7
Estanislau Trepat <estanis.trepat@inson.es>
2025-08-05 19:02:54 +0200

    Fix comments

--- install.d/30-apps-dbeaver.sh
+++ install.d/30-apps-dbeaver.sh
@@ -2,11 +2,7 @@
 
 # DBeaver Community - Free Universal Database Tool
 # We install a custom-made aur package (pkgbuild) so that it can be installed
-# in a self-contained way, with its own JDK.
-
-# FIXME
-# This is to install a fully self-contained dbeaver version so that Java is included and can coexist with anything
-# installed by the user. We should think about removing this script and use an AUR package instead or provide a PKGBUILD.
+# self-contained, with its own JDK.
 
 echo "=> Installing DBeaver..."
 
