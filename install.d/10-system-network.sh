91d6d70fc3c3c38274af8214833c2ecb3fde6fb2
Estanislau Trepat <estanis.trepat@inson.es>
2025-08-06 17:27:24 +0200

    Add iwd daemon if not installed automatically

---
+++ install.d/10-system-network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install iwd explicitly if it wasn't included in archinstall
+# This can happen if archinstall used ethernet
+if ! command -v iwctl &>/dev/null; then
+  yay -S --noconfirm --needed iwd
+  sudo systemctl enable --now iwd.service
+fi
