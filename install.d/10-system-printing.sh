9b583d41f7bb5e2bc7223380f3c284b54f066826
Estanislau Trepat <estanis.trepat@inson.es>
2025-08-07 11:09:50 +0200

    Add avahi package for easy printer discovery

--- install.d/10-system-printing.sh
+++ install.d/10-system-printing.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # Add printing services
-yay -S --noconfirm cups cups-pdf cups-filters
+yay -S --noconfirm avahi cups cups-pdf cups-filters
+sudo systemctl enable --now avahi-daemon.service
 sudo systemctl enable --now cups.service
