1f297c76095b9f33be150a5dbc76de04a90e24c0
Estanislau Trepat <estanis@etrepat.com>
2025-08-14 00:00:15 +0200

    Improve mpv initial config

--- install.d/30-apps-mpv.sh
+++ install.d/30-apps-mpv.sh
@@ -3,4 +3,9 @@
 # mpv - a free, open source, and cross-platform media player
 # https://mpv.io/
 echo "=> Installing mpv..."
-yay -S --noconfirm --needed mpv
+
+yay -S --noconfirm --needed mpv mpv-mpris
+
+cp -r /usr/share/doc/mpv/ ~/.config/
+sed -i 's/#profile=high-quality/profile=high-quality/' ~/.config/mpv/mpv.conf
+sed -i 's/#hwdec=auto/hwdec=auto/' ~/.config/mpv/mpv.conf
