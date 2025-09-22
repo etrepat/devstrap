e37baf8a51e8dca9a4c3ffd51f929e6c0e6b7eb1
Estanislau Trepat <estanis.trepat@inson.es>
2025-09-22 12:34:11 +0200

    Remove xclip favouring wl-clipboard & set eza theme

--- install.d/10-utils-term.sh
+++ install.d/10-utils-term.sh
@@ -3,4 +3,8 @@
 # Terminal utilities
 echo "=> Installing terminal utilities..."
 yay -Qq htop && yay -Rns --noconfirm htop
-yay -S --needed --noconfirm bat btop chafa eza fastfetch fd fzf jq tldr
+yay -S --needed --noconfirm bat btop chafa eza fastfetch fd fzf jq tldr wl-clipboard
+
+# Set eza theme
+mkdir -p ~/.config/eza
+cp -f "${DEVSTRAP_PATH}/config/eza.yml" ~/.config/eza/theme.yml
