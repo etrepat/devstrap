6c4e8e3a5dcf78e6ad0eef86a0e6b06090ef2e75
Estanislau Trepat <estanis.trepat@inson.es>
2026-08-27 18:53:35 +0200

    Make VS Code editor opt-in and cover all selected languages

--- install.d/25-dev-editor-vscode.sh
+++ install.d/25-dev-editor-vscode.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Visual Studio Code
+# Visual Studio Code (if selected)
+devstrap_prompt_editors
+if ! devstrap_editor_selected "Visual Studio Code"; then
+    return 0
+fi
+
 echo "=> Install Visual Studio Code..."
 yay -S --noconfirm --needed visual-studio-code-bin
 
@@ -9,7 +14,7 @@ mkdir -p ~/.config/Code/User
 [ -f "$HOME/.config/Code/User/settings.json" ] && mv ~/.config/Code/User/settings.json ~/.config/Code/User/settings.json.bak
 cp -f "${DEVSTRAP_PATH}/config/vscode.json" ~/.config/Code/User/settings.json
 
-# Install default extension set
+# Install base extension set
 echo "=> Installing base extensions..."
 code --install-extension enkia.tokyo-night
 code --install-extension vscode-icons-team.vscode-icons
@@ -18,20 +23,46 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension eamodio.gitlens
 code --install-extension mikestead.dotenv
 
-# Add lang extensions
+# Add language extensions for the selected languages
 echo "=> Installing language extensions (if selected)..."
 
+# Elixir
+if devstrap_lang_selected "Elixir"; then
+    code --install-extension JakeBecker.elixir-ls
+fi
+
 # Go
-if [[ -n "$DEVSTRAP_SELECTED_LANGS" && " $DEVSTRAP_SELECTED_LANGS " =~ [[:space:]]Go[[:space:]] ]]; then
+if devstrap_lang_selected "Go"; then
     code --install-extension golang.go
 fi
 
+# Java
+if devstrap_lang_selected "Java"; then
+    code --install-extension redhat.java
+fi
+
+# Node.js
+if devstrap_lang_selected "Node.js"; then
+    code --install-extension dbaeumer.vscode-eslint
+    code --install-extension esbenp.prettier-vscode
+fi
+
 # PHP (Intelephense)
-if [[ -n "$DEVSTRAP_SELECTED_LANGS" && " $DEVSTRAP_SELECTED_LANGS " =~ [[:space:]]PHP[[:space:]] ]]; then
+if devstrap_lang_selected "PHP"; then
     code --install-extension bmewburn.vscode-intelephense-client
 fi
 
-# Rust analyzer
-if [[ -n "$DEVSTRAP_SELECTED_LANGS" && " $DEVSTRAP_SELECTED_LANGS " =~ [[:space:]]Rust[[:space:]] ]]; then
+# Python
+if devstrap_lang_selected "Python"; then
+    code --install-extension ms-python.python
+fi
+
+# Ruby
+if devstrap_lang_selected "Ruby"; then
+    code --install-extension castwide.solargraph
+fi
+
+# Rust
+if devstrap_lang_selected "Rust"; then
     code --install-extension rust-lang.rust-analyzer
 fi
